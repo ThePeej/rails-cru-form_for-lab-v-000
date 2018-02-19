0fdc37af2eb1154fe48321827f209516bd6b7294
PJ Frias <peejfrias@gmail.com>
2018-02-19 23:31:17 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,4 +25,10 @@ class SongsController < ApplicationController
 
     redirect_to song_path(@song)
   end
+
+  private
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
 end
